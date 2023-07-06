df3acf8e825b83a15dcaa1882a8098820f052b57
thariya <twarnakula@gmail.com>
2023-07-07 02:15:31 +1000

    Added some customization for look and feel as well as C++ support

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -17,9 +17,10 @@ M.blankline = {
   },
   buftype_exclude = { "terminal" },
   show_trailing_blankline_indent = false,
-  show_first_indent_level = false,
+  show_first_indent_level = true,
   show_current_context = true,
   show_current_context_start = true,
+  char = '┊',
 }
 
 M.luasnip = function(opts)
